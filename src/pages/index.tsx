4b7c55012f25d07d0312d780a18822508d0fd8b0
정원 <jwha94@gmail.com>
2024-03-25 03:14:21 +0900

    feat: HomePage로 접근 시 /chart로 리다이렉트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
 export default function HomePage() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    navigate("/chart");
+  }, [navigate, pathname]);
+
   return <div>Home Page</div>;
 }
