96119412afb80eabde2b46b4c09b229b9789dddb
정원 <jwha94@gmail.com>
2024-03-25 18:27:54 +0900

    feat: msw 세팅 - whook

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,36 @@
+import { Content, DefaultContentData } from "@/models/contents";
+import { WHOOK } from "@/models/contents/whook";
 import { HttpResponse, http } from "msw";
 
-const testData = Array.from(Array(1024).keys()).map((id) => ({
-  id,
-  title: `Test Data ${id + 1}`,
-}));
+// const testData = Array.from(Array(1024).keys()).map((id) => ({
+//   id,
+//   title: `Test Data ${id + 1}`,
+// }));
+
+const generateContents = (
+  count: number,
+  data: DefaultContentData
+): Content[] => {
+  return Array.from({ length: count }, (_, id) => ({
+    ...data,
+    id,
+  }));
+};
 
 export const handlers = [
-  http.get("/test", () => {
-    return HttpResponse.json({ contents: testData }, { status: 200 });
+  http.get("/whook", () => {
+    const CONTENT_AMOUNT = 10;
+
+    const getRandomNumber = (index = 0) =>
+      Math.round(Math.random() * 100) + index;
+
+    const contents = generateContents(CONTENT_AMOUNT, WHOOK.DEFAULT_DATA).map(
+      (v, i) => ({
+        ...v,
+        amount: getRandomNumber(i),
+      })
+    );
+
+    return HttpResponse.json({ contents }, { status: 200 });
   }),
 ];
